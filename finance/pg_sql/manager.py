8bab3b7c67e1dad2cb5a7399d3d1e30046a751fa
nitinSharma290 <nitin.sharma@kwalee.com>
2024-04-14 18:17:29 +0530

    sql adapter added with more sql functions

--- finance/pg_sql/manager.py
+++ finance/pg_sql/manager.py
@@ -1,10 +1,13 @@
+from typing import List, Dict
+
 import psycopg2
+from psycopg2.extensions import cursor
 from psycopg2.extras import execute_values
 from psycopg2.pool import SimpleConnectionPool
 
+from finance.configs import DBTableNames
 from finance.pg_sql.context import PoolCtxManager
 from finance.pg_sql.connection_pooling import pool as pg_pool
-from finance.configs import DBTableNames
 
 
 class DBManager:
@@ -20,26 +23,41 @@ class DBManager:
     def put_connection_in_pool(self, conn):
         self._connection_pool.putconn(conn)
 
-    def execute_query(self, query: str):
-        with PoolCtxManager(self._connection_pool) as cursor:
-            cursor.execute(query)
+    @staticmethod
+    def get_formatted_output_from_cursor(cursor: cursor) -> List[Dict]:
+        if not cursor.description:
+            return []
+        columns = [i[0] for i in cursor.description]
+        rows = cursor.fetchall()
+        return [dict(zip(columns, i)) for i in rows]
 
-    def get(self, table_name: str, limit: int = 100, offset: int = 0):
+    def execute_query(self, query: str, values: tuple | list | None = None) -> None | list:
+        with PoolCtxManager(self._connection_pool) as cursor:
+            cursor.execute(query, values)
+            if cursor.description:
+                return cursor.fetchall()
+
+    def get_all(self, table_name: str, limit: int = 100, offset: int = 0):
         query = f"SELECT * FROM {table_name} LIMIT {limit} OFFSET {offset}"
 
         with PoolCtxManager(self._connection_pool) as cursor:
             cursor.execute(query)
-            return cursor.fetchall()
+            return self.get_formatted_output_from_cursor(cursor)
 
-    def update(self, table_name: str, data: dict, condition: str):
-        set_clause = ', '.join([f"{key} = %s" for key in data.keys()])
-
-        query = f"UPDATE {table_name} SET {set_clause}"
-        if condition:
-            query += f" WHERE {condition}"  # sql injection?
-
+    def get_by_id(self, table_name: str, id_value: str | int, id_key: str = 'object_id', limit: int = 1, offset: int = 0):
+        query = f"SELECT * FROM {table_name} WHERE {id_key} = %s LIMIT {limit} OFFSET {offset}"
         with PoolCtxManager(self._connection_pool) as cursor:
-            cursor.execute(query, list(data.values()))
+            cursor.execute(query, (id_value,))
+            return self.get_formatted_output_from_cursor(cursor)
+
+    def update_by_id(self, table_name: str,  id_value: str,  data: dict, id_key: str = 'id'):
+        set_clause = ', '.join([f"{key} = %s" for key in data.keys()])
+        query = f"UPDATE {table_name} SET {set_clause} WHERE {id_key} = %s"
+
+        values = list(data.values())
+        values.append(id_value)
+        with PoolCtxManager(self._connection_pool) as cursor:
+            cursor.execute(query, values)
 
     def insert(self, table_name: str, data: dict):
         columns = ', '.join(data.keys())
@@ -64,6 +82,19 @@ class DBManager:
         with PoolCtxManager(self._connection_pool) as cursor:
             execute_values(cursor, query, values)
             print("Data inserted successfully.")
+
+    def get_specific_field(self, table_name: str, field_name: str, limit: int = 100, offset: int = 0):
+        query = f"SELECT {field_name} FROM {table_name} LIMIT {limit} OFFSET {offset}"
+        with PoolCtxManager(self._connection_pool) as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
+
+    def delete_by_id(self, table_name: str, id_value: str, id_key: str = 'id'):
+        query = f"DELETE FROM {table_name} WHERE {id_key} = %s"
+        with PoolCtxManager(self._connection_pool) as cursor:
+            cursor.execute(query, (id_value,))
+
+
 
 
 
